eef1b563c30b9cc9576af2e6df0cbc59a1c49626
Mateusz Owczarek <getrajer533@gmail.com>
2021-01-28 22:34:53 +0000

    Counting price (BAD VERSION)

--- js/boostForm.js
+++ js/boostForm.js
@@ -8,7 +8,14 @@ const StartDivisionSelectValue = document.getElementById("startDivisionSelectVal
 const EndDivisionSelectType = document.getElementById("goalDivision");
 const EndDivisionSelectValue = document.getElementById("endDivisionSelectValue");
 
+const LessLPModifier = 15;
 
+const IronDivPrice = 15;
+const IronDivPromoPrice = 20;
+const BronzeDivPrice = 18;
+const BronzeDivPromoPrice = 23;
+const SilverDivPrice = 24;
+const SilverDivPromoPrice = 30;
 
 const reEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -61,37 +68,397 @@ const calculatePrice = () =>{
     let endDivisionTypeSelected = parseInt(EndDivisionSelectType.value);
     let endDivisionValueSelected = parseInt(EndDivisionSelectValue.value);
 
+    let ifLessLP = document.getElementById("lessLP").value;
 
     console.log(startDivisionTypeSelected + " | " + endDivisionTypeSelected)
     console.log(startDivisionValueSelected + " | " + endDivisionValueSelected)
 
+    document.getElementById("endDivisionSelectValue").style.display = "inline";
+    document.getElementById("startDivisionSelectValue").style.display = "inline";
 
-    if(startDivisionTypeSelected > endDivisionTypeSelected){
+    let price = 0;
+
+    if(startDivisionTypeSelected == 7){
+        document.getElementById("startDivisionSelectValue").style.display = "none";
+    }
+
+    if(endDivisionTypeSelected == 7){
+        document.getElementById("endDivisionSelectValue").style.display = "none";
+
+    }
+
+    if(startDivisionTypeSelected >= endDivisionTypeSelected && endDivisionValueSelected == 1){
         PriceDisplay.innerHTML = "0 ZŁ";
+        
     }
     else{
         switch(startDivisionTypeSelected){
             case 1:{
-                
                 switch(endDivisionTypeSelected){
                     case 1:{
-                        let price = (endDivisionValueSelected - startDivisionValueSelected) * 15;
+                        price = (endDivisionValueSelected - startDivisionValueSelected) * 15;
                         PriceDisplay.innerHTML = price + " ZŁ";
                         break;
                     }
                     case 2:{
-                        console.log(endDivisionValueSelected);
-                        let price = 20 + ((4 - startDivisionValueSelected) * 15);
+                        price = 20 + ((4 - startDivisionValueSelected) * 15);
                         if(endDivisionValueSelected != 1){
                             price += (endDivisionValueSelected - 1) * 18;
                         }
                         PriceDisplay.innerHTML = price + " ZŁ";
                         break;
                     }
-                }
+                    case 3:{
+                        price = 20 + ((4 - startDivisionValueSelected) * 15);
+                        price += 23 + (3 * 18);
+                        
+                        if(endDivisionValueSelected != 1){
+                            price += (endDivisionValueSelected - 1) * 24;
+                        }
 
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 4:{
+                        price = 20 + ((4 - startDivisionValueSelected) * 15);
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+
+                        if(endDivisionValueSelected != 1){
+                            price += (endDivisionValueSelected - 1) * 34;
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 5:{
+                        price = 20 + ((4 - startDivisionValueSelected) * 15);
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+                        price += 40 + (3 * 34);
+                        price += 75 + (3 * 45);
+
+                        if(endDivisionValueSelected != 1){
+                            price += (endDivisionValueSelected - 1) * 45;
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 6:{
+                        price = 20 + ((4 - startDivisionValueSelected) * 15);
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+                        price += 40 + (3 * 34);
+                        price += 75 + (3 * 45);
+
+                        if(endDivisionValueSelected != 1){
+                            if(endDivisionValueSelected === 2) {price += 100;}
+                            if(endDivisionValueSelected === 3) {price += 100 + 130;}
+                            if(endDivisionValueSelected === 4) {price += 100 + 130 + 150;}
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 7:{
+                        price = 20 + ((4 - startDivisionValueSelected) * 15);
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+                        price += 40 + (3 * 34);
+                        price += 75 + (3 * 45);
+                        price += 100 + 130 + 150;
+                        price += 300;
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        
+                        document.getElementById("endDivisionSelectValue").style.display = "none";
+
+                        break;
+                    }
+                }
+                break;
+            }
+            case 2:{
+                switch(endDivisionTypeSelected){
+                    case 2:{
+                        if(endDivisionValueSelected != 1){
+                            price += (endDivisionValueSelected - 1) * 18;
+                        }
+                        console.log("dsadsa");
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 3:{
+                        price += 23 + (3 * 18);
+                        
+                        if(endDivisionValueSelected != 1){
+                            price += (endDivisionValueSelected - 1) * 24;
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 4:{
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+
+                        if(endDivisionValueSelected != 1){
+                            price += (endDivisionValueSelected - 1) * 34;
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 5:{
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+                        price += 40 + (3 * 34);
+                        price += 75 + (3 * 45);
+
+                        if(endDivisionValueSelected != 1){
+                            price += (endDivisionValueSelected - 1) * 45;
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 6:{
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+                        price += 40 + (3 * 34);
+                        price += 75 + (3 * 45);
+
+                        if(endDivisionValueSelected != 1){
+                            if(endDivisionValueSelected === 2) {price += 100;}
+                            if(endDivisionValueSelected === 3) {price += 100 + 130;}
+                            if(endDivisionValueSelected === 4) {price += 100 + 130 + 150;}
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 7:{
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+                        price += 40 + (3 * 34);
+                        price += 75 + (3 * 45);
+                        price += 100 + 130 + 150;
+                        price += 300;
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        
+                        document.getElementById("endDivisionSelectValue").style.display = "none";
+
+                        break;
+                    }
+                }
+                break;
+            }
+            case 3:{
+                switch(endDivisionTypeSelected){
+                    case 3:{
+                        if(endDivisionValueSelected != 1){
+                            price += (endDivisionValueSelected - 1) * 24;
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 4:{
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+
+                        if(endDivisionValueSelected != 1){
+                            price += (endDivisionValueSelected - 1) * 34;
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 5:{
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+                        price += 40 + (3 * 34);
+                        price += 75 + (3 * 45);
+
+                        if(endDivisionValueSelected != 1){
+                            price += (endDivisionValueSelected - 1) * 45;
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 6:{
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+                        price += 40 + (3 * 34);
+                        price += 75 + (3 * 45);
+
+                        if(endDivisionValueSelected != 1){
+                            if(endDivisionValueSelected === 2) {price += 100;}
+                            if(endDivisionValueSelected === 3) {price += 100 + 130;}
+                            if(endDivisionValueSelected === 4) {price += 100 + 130 + 150;}
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 7:{
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+                        price += 40 + (3 * 34);
+                        price += 75 + (3 * 45);
+                        price += 100 + 130 + 150;
+                        price += 300;
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        
+                        document.getElementById("endDivisionSelectValue").style.display = "none";
+
+                        break;
+                    }
+                }
+                break;
+            }
+            case 4:{
+                switch(endDivisionTypeSelected){
+                    case 4:{
+                        if(endDivisionValueSelected != 1){
+                            price += (endDivisionValueSelected - 1) * 34;
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 5:{
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+                        price += 40 + (3 * 34);
+                        price += 75 + (3 * 45);
+
+                        if(endDivisionValueSelected != 1){
+                            price += (endDivisionValueSelected - 1) * 45;
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 6:{
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+                        price += 40 + (3 * 34);
+                        price += 75 + (3 * 45);
+
+                        if(endDivisionValueSelected != 1){
+                            if(endDivisionValueSelected === 2) {price += 100;}
+                            if(endDivisionValueSelected === 3) {price += 100 + 130;}
+                            if(endDivisionValueSelected === 4) {price += 100 + 130 + 150;}
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 7:{
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+                        price += 40 + (3 * 34);
+                        price += 75 + (3 * 45);
+                        price += 100 + 130 + 150;
+                        price += 300;
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        
+                        document.getElementById("endDivisionSelectValue").style.display = "none";
+
+                        break;
+                    }
+                }
+                break;
+            }
+            case 5:{
+                switch(endDivisionTypeSelected){
+                    case 5:{
+                        if(endDivisionValueSelected != 1){
+                            price += (endDivisionValueSelected - 1) * 45;
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 6:{
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+                        price += 40 + (3 * 34);
+                        price += 75 + (3 * 45);
+
+                        if(endDivisionValueSelected != 1){
+                            if(endDivisionValueSelected === 2) {price += 100;}
+                            if(endDivisionValueSelected === 3) {price += 100 + 130;}
+                            if(endDivisionValueSelected === 4) {price += 100 + 130 + 150;}
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 7:{
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+                        price += 40 + (3 * 34);
+                        price += 75 + (3 * 45);
+                        price += 100 + 130 + 150;
+                        price += 300;
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        
+                        document.getElementById("endDivisionSelectValue").style.display = "none";
+
+                        break;
+                    }
+                }
+                break;
+            }
+            case 6:{
+                switch(endDivisionTypeSelected){
+                    case 6:{
+                        if(endDivisionValueSelected != 1){
+                            if(endDivisionValueSelected === 2) {price += 100;}
+                            if(endDivisionValueSelected === 3) {price += 100 + 130;}
+                            if(endDivisionValueSelected === 4) {price += 100 + 130 + 150;}
+                        }
+
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        break;
+                    }
+                    case 7:{
+                        price += 23 + (3 * 18);
+                        price += 30 + (3 * 24);
+                        price += 40 + (3 * 34);
+                        price += 75 + (3 * 45);
+                        price += 100 + 130 + 150;
+                        price += 300;
+                        
+                        PriceDisplay.innerHTML = price + " ZŁ";
+                        
+                        document.getElementById("endDivisionSelectValue").style.display = "none";
+
+                        break;
+                    }
+                }
+                break;
+            }
+            case 7:{
+                PriceDisplay.innerHTML = 0 + " ZŁ";
+                document.getElementById("endDivisionSelectValue").style.display = "none";
+                document.getElementById("startDivisionSelectValue").style.display = "none";
+                break;
             }
         }
+
+        if(ifLessLP == "on"){
+            price += Math.round((price * LessLPModifier / 100));
+        }
+        PriceDisplay.innerHTML = price + " ZŁ";
     }
 }
 
